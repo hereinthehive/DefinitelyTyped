c47d1116d6f7ff3d3dc8e215c1283ba36a0127e9
Dan Donald <hereinthehive@dans-mbp.lan>
2023-04-03 18:40:36 +0100

    Fixed based on linting feedback

---
+++ types/design-tokens/design-tokens-tests.ts
@@ -0,0 +1,73 @@
+import { CompositeTokenType, SingleTokenType, TokenGroup, TokenItem, TokenItemObject } from "design-tokens";
+import DesignTokens = require("design-tokens");
+
+const testTokenObject: TokenItemObject = {
+    $type: SingleTokenType.Color,
+    $value: "#f00",
+    $description: "This is a test"
+};
+
+const testTokenObjectWithNumber: TokenItemObject = {
+    $type: SingleTokenType.FontWeight,
+    $value: 400,
+};
+
+// yes, you wouldn't set an array for a font weight ;)
+const testTokenObjectWithArray: TokenItemObject = {
+    $type: SingleTokenType.FontWeight,
+    $value: [400, 500],
+};
+
+const testCompositeTokenObject: TokenItemObject = {
+    $type: CompositeTokenType.Shadow,
+    $value: {
+      color: "#00000088",
+      offsetX: "0.5rem",
+      offsetY: "0.5rem",
+      blur: "1.5rem",
+      spread: "0rem"
+    },
+};
+
+const testColorToken: TokenItem = {
+  "test-color": {
+    $type: SingleTokenType.Color,
+    $value: "#f00"
+  }
+};
+
+const testColorTokenGroup: TokenGroup = {
+  color: {
+    dark: {
+      $value: "#1B283A",
+      $type: SingleTokenType.Color
+    },
+    base: {
+      $value: "#222B5E",
+      $type: SingleTokenType.Color
+    },
+    light: {
+      $value: "#4871D9",
+      $type: SingleTokenType.Color
+    }
+  }
+};
+
+const testColorTokenNestedGroup: TokenGroup = {
+  color: {
+    blue: {
+      dark: {
+        $value: "#1B283A",
+        $type: SingleTokenType.Color
+      },
+      base: {
+        $value: "#222B5E",
+        $type: SingleTokenType.Color
+      },
+      light: {
+        $value: "#4871D9",
+        $type: SingleTokenType.Color
+      }
+    }
+  }
+};
---
+++ types/design-tokens/index.d.ts
@@ -0,0 +1,66 @@
+// Type definitions for design-tokens 1.0
+// Project: https://second-editors-draft.tr.designtokens.org/format/
+// Definitions by: Dan Donald <https://github.com/hereinthehive>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare namespace DesignTokens {
+  /**
+   * These are the basic types derved from the spec
+   */
+  enum SingleTokenType {
+    Color = "color",
+    Dimension = "dimension",
+    Duration = "duration",
+    FontFamily = "fontFamily",
+    FontWeight = "fontWeight",
+    Number = "number",
+  }
+
+  /**
+   * Composite types are also defined in the space
+   */
+  enum CompositeTokenType {
+    Border = "border",
+    CubicBezier = "cubicBezier",
+    Gradient = "gradient",
+    Shadow = "shadow",
+    StrokeStyle = "strokeStyle",
+    Transition = "transition",
+    Typography = "typography"
+  }
+
+  /**
+   * To ensure that the $value of a composite object is clear what it can store
+   */
+  interface CompositeTokenItemValue {
+    [key: string]: string | number;
+  }
+
+  /**
+   * The item object described the content of a specific token
+   */
+  interface TokenItemObject {
+    "$type": SingleTokenType | CompositeTokenType;
+    "$value": number | number[] | string |  string[] | CompositeTokenItemValue;
+    "$description"?: string;
+    "$extensions"?: object;
+  }
+
+  /**
+   * A specific token is named by it's key
+   */
+  interface TokenItem {
+    [name: string]: TokenItemObject;
+  }
+
+  /**
+   * A token group is a collection of design tokens
+   *
+   * TODO: add optional $description filed
+   */
+  interface TokenGroup {
+    [name: string]: TokenGroup | TokenItem | TokenItemObject;
+  }
+}
+
+export = DesignTokens;
